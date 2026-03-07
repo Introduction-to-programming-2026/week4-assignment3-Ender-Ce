135dff33fd2617574c42b3ef700ae60f611f1018
Ender-Ce <aray.ender@ibu.edu.mk>
2026-03-07 03:30:49 +0000

    Sat, Mar 7, 2026, 4:30 AM +01:00

--- src/iteration.c
+++ src/iteration.c
@@ -30,5 +30,17 @@ int main() {
     int n;
 
     do{
-        printf
+        printf("%d", &n);
+    } while (n < 1);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("#");
     }
+printf("\n");
+    }
+    return 0;
+}
+
+
+
